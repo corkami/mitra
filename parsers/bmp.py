02f06f98369872e07c23186b324898e004cfef12
Ange <ange.albertini@gmail.com>
2021-01-20 21:05:42 +0100

    BMP cleanup

--- parsers/bmp.py
+++ parsers/bmp.py
@@ -9,27 +9,29 @@ class parser(FType):
 	TYPE = "BMP"
 	MAGIC = b"BM"
 
+	# whole file size (not necessarily correct nor validated)
+	SIZE_o = 2
+	# pointer to bitmap data - after the whole header.
+	OFFSET_o = 0xA
+
+
 	def __init__(self, data=""):
 		FType.__init__(self, data)
 		self.data = data
 
 		self.bParasite = True
-		self.cut = 0x40
-		self.parasite_o = 0x40
+		self.parasite_o = 14 + 12 # minimum in OS/2 1.x - BitmapFileHeader + BitmapCoreHeader 
 		self.parasite_s = 0xFFFFFFFF - 1
+		self.cut = self.parasite_o 
 
 
 	def getCut(self):
-		self.cut = get4l(self.data, 0xA)
+		self.cut = get4l(self.data, self.OFFSET_o)
 		return self.cut
 
 
 	def fixformat(self, d, delta):
-		# `BM` SSSS ???? OOOO
-		size_o = 2
-		d = inc4l(d, size_o, delta)
-
-		offset_o = 0xA
-		d = inc4l(d, offset_o, delta)
+		d = inc4l(d, self.SIZE_o, delta)
+		d = inc4l(d, self.OFFSET_o, delta)
 
 		return d
