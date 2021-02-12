a3e83df138cd71b06c5487c63e254acc0675056b
Ange <ange.albertini@gmail.com>
2021-02-12 17:14:30 +0100

    Gzip: parasite as a subfield of the extra field.

--- parsers/gzip.py
+++ parsers/gzip.py
@@ -2,6 +2,8 @@
 
 # GZIP
 
+# Unsupported: if an extra field is already present.
+
 from parsers import FType
 from helpers import *
 
@@ -11,28 +13,46 @@ class parser(FType):
 	TYPE = "GZ"
 	MAGIC = b"\x1f\x8b"
 
+
 	def __init__(self, data=""):
 		FType.__init__(self, data)
 		self.data = data
-		self.start_o = 0 # yes, it's required 
+		self.start_o = 0 # yes, it's required
 
 		self.bParasite = True
-		self.parasite_o = 12 # as extra field 
-		self.parasite_s = 0xFFFF
+		self.hack = False # as a subfield or directly in the extra field
 
 		self.cut = 10
 		self.prewrap = 2
+		if not self.hack:
+			self.prewrap = 6
+		else:
+			self.prewrap = 2
 
-
-	def wrap(self, data):
-		return b"".join([
-			int2l(len(data)),
-			data,
-		])
+		self.parasite_o = self.cut + self.prewrap
+		self.parasite_s = 0xFFFF
 
 
 	def fixformat(self, d, delta):
+		# set the extra field flag
 		flags = d[3]
 		flags += 1 << 2
 
 		return d[:3] + bytes([flags]) + d[4:]
+
+
+	def wrap(self, data):
+		# the extra field is made of TLV subfields
+		if not self.hack:
+			extra = b"".join([
+				b"Id",             # Type
+				int2l(len(data)),  # Length
+				data,              # Value
+			])
+		else:
+			extra = data
+
+		return b"".join([
+			int2l(len(extra)),
+			extra,
+		])
