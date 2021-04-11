f20c804953dbc978923f49106276dc77c8471b61
Ange <ange.albertini@gmail.com>
2021-04-11 16:26:20 +0200

    AngeCryption scripts and examples

---
+++ cbc/README.md
@@ -0,0 +1,2 @@
+A tool to generate from a Mitra polyglot
+a file that decrypts to another valid file via CBC (AngeCryption)
---
+++ cbc/decrypt.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import sys
+
+import os
+import hashlib
+import binascii
+from Crypto.Cipher import AES
+
+
+if __name__=='__main__':
+	fname = sys.argv[1]
+	with open(fname, "rb") as f:
+		lines = f.readlines()
+
+	for line in lines:
+		line = line.strip()
+		l = line.split(b": ")
+		if l[1].startswith(b"b'") and l[1][-1] == 39:
+			l[1] = l[1][2:-1]
+		vars()[l[0].decode("utf-8").lower()] = l[1].strip().decode("utf-8")
+
+	key_s = key
+	iv_s = iv
+	for v in ["key", "iv", "plaintext"]:
+		vars()[v] = binascii.unhexlify(vars()[v])
+
+
+	cbc = AES.new(key, AES.MODE_CBC, iv)
+	ciphertext = cbc.encrypt(plaintext)
+
+
+	hash_ = hashlib.sha256(plaintext).hexdigest()[:8].lower()
+
+	fname = os. path.splitext(fname)[0] # remove file extension
+
+	exts = exts.split(" ")[-2:]
+	fn1 = "%s.%s.%s" % (fname, hash_, exts[0])
+	with open(fn1, "wb") as file1:
+		file1.write(plaintext)
+
+	fn2 = "%s.%s.%s" % (fname, hash_, exts[1])
+	with open(fn2, "wb") as file2:
+		file2.write(ciphertext)
+
+	print("key:", key.rstrip(b"\0"))
+	print("iv:", iv_s)
+	print()
+	print("plaintext1:", binascii.hexlify(plaintext[:32]),"...")
+	print("plaintext2:", binascii.hexlify(ciphertext[:32]),"...")
+	print()
+	print("Test:")
+	print(" openssl enc -in %s -out %s -aes-128-cbc -iv %s -K %s" % (fn1, fn2, iv_s, key_s))
